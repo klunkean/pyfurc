1f96b526cb34cc4764bb839acaf7e3d0d1b11240
Andre Sebastian Klunker <a.klunker@tu-berlin.de>
2021-06-10 11:45:45 +0200

    Bump version: 0.1.2 → 0.1.3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Andre Klunker"
 author = "Andre Klunker"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
