58e894b3721e2c09b29fc1dc691b1d2fd0a88516
klunkean <klunkean@posteo.de>
2021-07-05 11:48:16 +0200

    add unit tests for config file and for running auto

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,19 @@
 import pytest
 import pyfurc as pf
+import sympy as sp
 
 @pytest.fixture()
 def bf_problem():
-    """Standard BifurcationProblem fixture for testing. Uses hinged cantilever, i.e. symmetric bifurcation.
+    """Standard BifurcationProblem fixture for testing. Uses hinged cantilever, i.e. symmetric bifurcation with normalized parameters.
     """
-    ...
+    phi = pf.PhysicalQuantity("\\varphi", quantity_type="DOF")
+    P = pf.PhysicalQuantity("P", quantity_type="load")
+    
+    # first bifurcation point will be at exactly P = 1, phi = 0
+    V = pf.Energy(1/2*phi**2-P*(1-sp.cos(phi)))
+    
+    bf = pf.BifurcationProblem(V, name="hinged_cantilever")
+    # Set maximum load to double the critical load
+    bf.set_parameter("RL1", 2.)
+    
+    return bf
