618e6e1f033e040cc05cfb11f31f243d705373b0
Pablo Prietz <pablo@prietz.org>
2021-10-22 17:41:16 +0200

    Overwrite bdist_wheel to configure+make auto-07p

---
+++ setup.py
@@ -0,0 +1,13 @@
+import subprocess
+
+from setuptools import setup
+from wheel.bdist_wheel import bdist_wheel
+
+
+class _bdist_wheel(bdist_wheel):
+    def run(self):
+        subprocess.check_output(["scripts/build-auto-07p.sh"])
+        super().run()
+
+
+setup(cmdclass={"bdist_wheel": _bdist_wheel})
