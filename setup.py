ae801cb3dfb2a42600af3086774227e6853a8023
Pablo Prietz <pablo@prietz.org>
2021-10-24 20:42:46 +0200

    Use bash command to call build script

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ from wheel.bdist_wheel import bdist_wheel
 
 class _bdist_wheel(bdist_wheel):
     def run(self):
+        import os
+
+        print(f"{os.getcwd()=}")
         subprocess.check_call(["pwd"])
         subprocess.check_call(["ls", "-lha", ".", "scripts"])
-        subprocess.check_call(["scripts/build-auto-07p.sh"])
+        subprocess.check_call(["bash", "scripts/build-auto-07p.sh"])
         super().run()
 
 
