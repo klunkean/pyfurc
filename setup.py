313bbdbc1090bd4c905b0abe3fff8c21ee9a4022
Pablo Prietz <pablo@prietz.org>
2021-10-24 22:28:10 +0200

    Use cibuildwheel to build and audit wheels

--- setup.py
+++ setup.py
@@ -9,5 +9,16 @@ class _bdist_wheel(bdist_wheel):
         subprocess.check_call(["bash", "-x", "scripts/build-auto-07p.sh"])
         super().run()
 
+    def finalize_options(self):
+        super().finalize_options()
+        # Mark us as not a pure python package
+        self.root_is_pure = False
+
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+        # We just call external commands, no need to specify the python minor version
+        python, abi = "py3", "none"
+        return python, abi, plat
+
 
 setup(cmdclass={"bdist_wheel": _bdist_wheel})
