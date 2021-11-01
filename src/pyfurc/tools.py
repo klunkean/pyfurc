2b7306b1a95bb2af823ab3413cee565ec5c1d88c
klunkean <klunkean@posteo.de>
2021-11-01 15:30:19 +0100

    Discover auto-07p after new build process.

--- src/pyfurc/tools.py
+++ src/pyfurc/tools.py
@@ -9,15 +9,29 @@ def setup_auto_exec_env():
     if "AUTO_DIR" in env:
         auto_dir = env["AUTO_DIR"]
     else:
-        with importlib.resources.path(__package__, "auto-07p") as auto_dir_path:
-            auto_dir = str(auto_dir_path)
+        # This seems hacky. Don't know how else to find pyfurc-ext when
+        # it looks like this:
+        # |{base_path, e.g. site-packages}
+        #    |-- pyfurc
+        #       |-- __init__.py
+        #       |-- ...
+        #    |-- pyfurc-ext
+        #       |-- auto-07p
+
+        with importlib.resources.path(
+            __package__, "__init__.py"
+        ) as pkg_path:
+            auto_dir = pkg_path.parents[1].joinpath(
+                "pyfurc.ext", "auto-07p"
+            )
+
         env["AUTO_DIR"] = auto_dir
 
     with open(os.path.join(auto_dir, "cmds", "auto.env.sh")) as envf:
         for line in envf.readlines():
             if line.startswith("PATH"):
                 extend = line.split("=")[-1].rstrip()
-                extend = extend.replace("$AUTO_DIR", auto_dir)
+                extend = extend.replace("$AUTO_DIR", str(auto_dir))
                 newpath = env["PATH"] + f":{extend}"
                 env["PATH"] = newpath
     return env
