2c1c0819640e947f2349278a44d117a3bd6a7b3f
klunkean <klunkean@posteo.de>
2021-11-02 10:14:31 +0100

    Fix typo in comment.

--- src/pyfurc/tools.py
+++ src/pyfurc/tools.py
@@ -11,11 +11,11 @@ def setup_auto_exec_env():
     else:
         # This seems hacky. Don't know how else to find pyfurc-ext when
         # it looks like this:
-        # |{base_path, e.g. site-packages}
+        # {base_path, e.g. site-packages}
         #    |-- pyfurc
         #       |-- __init__.py
         #       |-- ...
-        #    |-- pyfurc-ext
+        #    |-- pyfurc.ext
         #       |-- auto-07p
 
         with importlib.resources.path(
