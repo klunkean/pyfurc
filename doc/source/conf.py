fe3fa22cc5fea2b5e3ee4c93dffc9c8bff19b59c
klunkean <klunkean@posteo.de>
2022-03-18 15:09:07 +0100

    Bump version: 0.2.1 → 0.2.2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, klunkean"
 author = "klunkean"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.1"
+release = "0.2.2"
 
 
 # -- General configuration ---------------------------------------------------
--- src/pyfurc/__init__.py
+++ src/pyfurc/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "ak"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 import configparser
 import os
 import warnings
